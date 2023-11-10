80ddc65ce09592e4d3f599014d596da415d18f78
comus3 <come.plantin.carrenard@gmail.com>
2023-11-10 15:59:58 +0100

    upd8

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 let express = require('express');
 
+
+
+//creer la liste
+
+let taskList = [];
+//mettre les elems exemple
+taskList.push("MANGER 1ere fois");
+taskList.push("MAnger ENCOREREE");
+taskList.push("Miam manger 3eme fois");
+
 let app = express();
 app.use(express.static('public'));
 app.get('/', (request,response)=>{
-    response.render('home.ejs');
-
+    ans1 = request.query.task
+    an2 = request.query.delete
+    console.log(ans1);
+    taskList.push(ans1)
+    response.render('home.ejs', { taskList : taskList });
 })
 app.listen(3000, function(){
-    console.log("Server ok")
+    console.log("Server ok");
 });
+
+//// Page LOgic
+
+
 
