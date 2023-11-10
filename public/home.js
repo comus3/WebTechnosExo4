1a4deedc956ff66b139bcf2cbd2e6b467e2e5d9b
comus3 <come.plantin.carrenard@gmail.com>
2023-11-10 13:20:17 +0100

    almost there

--- public/home.js
+++ public/home.js
@@ -31,7 +31,7 @@ class TaskList {
 }
 //creer la liste
 
-const taskList = new TaskList();
+var taskList = new TaskList();
 //mettre les elems exemple
 taskList.addTask("MANGER");
 taskList.addTask("MAnger ENCOREREE");
@@ -39,20 +39,27 @@ taskList.addTask("Miam manger ajd");
 
 //gestion du bouton add task
 var button = document.getElementById("button");
-button.addEventListener("click", addTask());
+button.addEventListener("click", function(){
+    addTask();
+})
 
 
-function updateTasks(){
+function updateTasks(taskList){
     var taskListElement = document.getElementById('taskList');
     taskListElement.textContent = ""
-    taskList.getTasks().array.forEach(element => {
-        taskListElement.appendChild(element)
+    index = 0
+    taskList.getTasks().forEach(element => {
+        var attribut = document.createElement("div");
+        attribut.textContent = element.returnTask();
+        taskListElement.appendChild(attribut);
         var deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.onclick = function() {
-            taskList.removeChild(newTask);
+            taskList.deleteTask(index);
+            updateTasks();
         };
-        newTask.appendChild(deleteButton);
+        taskListElement.appendChild(deleteButton);
+        index = index + 1;
     });
 
 }
@@ -64,5 +71,6 @@ function addTask() {
         taskList.addTask(taskInput)
         taskInput.value = '';
     }
+    updateTasks(taskList)
 }
 
